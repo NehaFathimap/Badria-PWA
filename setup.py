179c1c19fe6190c2bec94cbcaaa5fd9ab4c59bdc
Badria PWA <nah@enfono.com>
2026-04-21 13:54:02 +0000

    fix: Add setup.py so bench get-app resolves app name correctly

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="badria_pwa",
+    version="0.0.1",
+    description="Badria Van Sales PWA for ERPNext",
+    author="Enfono",
+    author_email="nah@enfono.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
